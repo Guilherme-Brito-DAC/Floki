e8265f97381c58c9b80130bb673ec3b5ed8f6998
Ronald Tadeu Alves Pinto Filho <50664977+julado123br@users.noreply.github.com>
2021-07-28 00:18:54 -0300

    funcoes de bancos de dados

--- backend/bd_connection.js
+++ backend/bd_connection.js
@@ -9,7 +9,6 @@ let config = {
   
 let connection = mysql.createConnection(config);
 
-
 function incluir(tabela, campos, dados){
 
     let sql = "INSERT INTO "+ tabela +" ("+ campos +") VALUES("+ dados +")";
@@ -17,23 +16,28 @@ function incluir(tabela, campos, dados){
     connection.end();
 }
 
-function alterar(tabela, campos, dados){
+function alterar(tabela, data, key){
 
-    let sql = "INSERT INTO "+ tabela +" ("+ campos +") VALUES("+ dados +")";
+    let sql = "UPDATE "+ tabela +" SET "+ data +" WHERE "+ key;
     connection.query(sql);
     connection.end();
 }
 
-function mostra(tabela, $key, campos = false, todos = true, ordem = false){
+function mostra(tabela, key, campos = false, todos = true, ordem = false){
+    let sql;
 
-    let sql = "INSERT INTO "+ tabela +" ("+ campos +") ";
+    (campos) ? sql = "SELECT "+ campos : sql = "SELECT * ";
+    sql += "FROM "+ tabela;
+    (!todos) ? sql += "LIMIT 1" : "";
+    (ordem) ? sql += "ORDEM BY "+ key + " DESC" : " ORDEM BY "+ key +" ASC";
+
     connection.query(sql);
     connection.end();
 }
 
-function excluir(key, condicao = false){
+function excluir(tabela, key){
 
-    let sql = "INSERT INTO "+ tabela +" ("+ campos +") VALUES("+ dados +")";
+    let sql = "DELETE FROM "+ tabela +" WHERE "+ key;
     connection.query(sql);
     connection.end();
 }
